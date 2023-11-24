ebacbcf89a90e9b8e1a47e621ed45814c61eb629
Nethsara <nethsarasiyum2@gmail.com>
2023-11-24 10:25:03 +0530

    Initial commit

---
+++ src/main/java/me/siyum/services/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package me.siyum.services.impl;
+/********************************************************************
+ * --
+ * Author: siyum
+ * Website: https://siyum.me
+ * Date: 24/11/23
+ * --
+ ********************************************************************/
+
+public class UserServiceImpl {
+}
